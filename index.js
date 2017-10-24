4b0b4dff7fdac174968d968971c04498643e0d75
Joseph Mawson <jmawson95@gmail.com>
2017-10-24 17:48:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 const app = "I don't do much."
+
+var kittens = [
+  "Milo", "Otis", "Garfield"
+]
+
+function destructivelyAppendKitten(array, name) {
+  var name = "Ralph";
+  kittens.push(name);
+  return kittens
+}
+
+function destructivelyPrependKitten(array, name) {
+  var name = "Bob";
+  kittens.unshift(name);
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(array, name) {
+  var name = "Ralph";
+  kittens.pop(name);
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(array, name) {
+  var name = "Bob";
+  kittens.shift(name);
+  return kittens
+}
+
+function appendKitten(array, name) {
+  var name = "Broom";
+  var new_array = [...array, name]
+  return new_array
+}
+
+function prependKitten(array, name) {
+  var name = "Arnold";
+  var new_array = [name, ...array]
+  return new_array
+}
+
+function removeLastKitten(array, name) {
+  var name = "Ralph";
+  kittens.pop(name);
+  return kittens
+}
+
+function removeFirstKitten(array, name) {
+  var name = "Bob";
+  kittens.shift(name);
+  return kittens
+}
